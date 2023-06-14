59fdfcd2bc1a2759af0cd72c38469d78f05c4204
Joana da Matta <108022074+jodamatta@users.noreply.github.com>
2023-06-14 07:58:54 -0300

    Update Q2.R

--- T2/Q2.R
+++ T2/Q2.R
@@ -1,7 +1,8 @@
 # Joana da Matta Furtado Ferreira - 2110255
 
 gera.variaveis <- function(funcao, nsamples){
-  u = runif(1)
+  X <- rep(0,nsamples)
+  U <- runif(nsamples)
   
   # caso 1: f(x)
   # a funcao de distribuicao acumulada de f(x) é F(X) = Int(3x^2, 0..x) = x^3, 0 <= x <= 1
@@ -9,22 +10,29 @@ gera.variaveis <- function(funcao, nsamples){
   
   if(funcao == 1){
     for(i in 1:nsamples){
-      return(u^(1/3))
+      X[i] <- U[i]^(1/3)
     }
   }
   
   # caso 2: g(x)
   # a funcao de distribuicao acumulada de g(x) é Int(4x,0..x), 0 <= x <= 0.5 e Int(4x, 0..0.4)+Int(4(1-x),0.4..x), 0.4<=x<=1
   # então, F(x) é (4x^2)/2 = 2x^2, 0<=x<=0.4, e 0.32 + (-2x^2 + 4x - 1.28) = -2x^2 + 4x - 0.96, 0.4 < x <= 1
-  # as inversas de F(x) sao: sqrt(x/2), 0 <= x <= 0.4 e 
+  # as inversas de F(x) sao: sqrt(x/2), 0 <= x <= 0.4 e - (-400+sqrt(-80000x+83200))/400
   
   else if(funcao == 2){
-    if(U[i] <= 0.4){
-      return(sqrt(u/2))
-    }else{
-        #X[i] <-
+    for(i in 1:nsamples){
+      if(U[i] <= 0.4){
+        X[i] <- sqrt(U[i]/2)
+      }else{
+        X[i] <- -(400+sqrt(-80000*U[i]+83200))/400
+      }
     }
   }
+  return(X)
 }
 
+var.fx <- gera.variaveis(1,10000)
+hist(var.fx)
 
+var.gx <- gera.variaveis(2,10000)
+hist(var.gx)
